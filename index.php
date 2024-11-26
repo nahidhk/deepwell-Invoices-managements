6077f2b204787ab23b1ce4ca8e78d0cdac5af00d
nahidhk <nahidappltd@gmail.com>
2024-11-26 23:31:34 +0600

    new update

--- index.php
+++ index.php
@@ -11,8 +11,10 @@
     <section class='flex anaround'>
         <div class="row  wm  left ">
         <?php require_once('./components/apply.html') ?>
+    <?php require_once('./components/cal.html') ?>
         </div>
     </section>
+
     <script src="app.js"></script>
     <script src="login.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/js/all.min.js" integrity="sha512-1JkMy1LR9bTo3psH+H4SV5bO2dFylgOy+UJhMus1zF4VEFuZVu5lsi4I6iIndE4N9p01z1554ZDcvMSjMaqCBQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
