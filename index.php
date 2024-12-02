48b07a8726221117574ee8930adce34b3477ca5f
nahidhk <nahidappltd@gmail.com>
2024-12-02 18:02:50 +0600

    new update

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <?php require_once('./components/cal.html') ?>
         </div>
     </section>
-
+<h1>test today done</h1>
     <script src="app.js"></script>
     <script src="login.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/js/all.min.js" integrity="sha512-1JkMy1LR9bTo3psH+H4SV5bO2dFylgOy+UJhMus1zF4VEFuZVu5lsi4I6iIndE4N9p01z1554ZDcvMSjMaqCBQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
